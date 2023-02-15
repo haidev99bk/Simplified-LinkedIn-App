215dcbc19f53217369e237dd0c7f2d86f2a7cf4e
haidev99bk <haidev99bk@gmail.com>
2023-02-16 00:52:35 +0700

    implement follower articles

---
+++ src/components/molecules/UserCard/UserCard.js
@@ -0,0 +1,30 @@
+import {View, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import styles from './styles';
+import AvatarIcon from '../../atoms/AvatarIcon';
+import fontWeight from '../../../styles/fontWeight';
+import {typography} from '../../../styles/typography';
+
+export default function UserCard(props) {
+  const {name, age, avatar, address, email, onPress} = props;
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <View style={styles.avatarContainer}>
+        <AvatarIcon source={{uri: avatar}} size={60} />
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={[fontWeight.bold, typography.header1]}>{name}</Text>
+        <Text style={[fontWeight.light, typography.typography]}>
+          {'Age: ' + age}
+        </Text>
+        <Text style={[fontWeight.light, typography.typography]}>
+          {'Address: ' + address}
+        </Text>
+        <Text style={[fontWeight.light, typography.typography]}>
+          {'Email: ' + email}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
